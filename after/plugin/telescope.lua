3c6d7540a00abdca18a3ebb4c817be927b41d869
Pavel Egorov <pavel.egorov@hodlhodl.com>
2023-07-22 00:37:57 +0300

    a

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -11,10 +11,10 @@ telescope.setup {
 }
 
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>?', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 
-vim.keymap.set('n', '<leader>?', function()
+vim.keymap.set('n', '<leader>/', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
